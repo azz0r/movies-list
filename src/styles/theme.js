53aff206b015720ba6c08d784b0128975b314d69
Aaron Lote <5314860+azz0r@users.noreply.github.com>
2018-08-20 21:50:54 +0100

    Add theme support

---
+++ src/styles/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+  fontSizes: {
+    small: "1rem",
+    medium: "1.5rem",
+    large: "2rem"
+  },
+  spacings: {
+    small: ".5rem",
+    medium: ".8rem",
+    large: "1rem"
+  },
+  palette: {
+    main: {
+      color: "white",
+      bg: "black",
+      fontFamily: "verdana"
+    },
+    article: {
+      color: "black",
+      bg: "#ccc",
+      fontFamily: "verdana"
+    }
+  }
+};
+
+export default theme;
