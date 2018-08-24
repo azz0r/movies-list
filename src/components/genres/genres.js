09563e255867c6db6e08abe203bb758db7e0f225
Aaron Lote <5314860+azz0r@users.noreply.github.com>
2018-08-24 20:12:24 +0100

    Style component genres

--- src/components/genres/genres.js
+++ src/components/genres/genres.js
@@ -1,7 +1,11 @@
 import React from "react";
 
+import { GenresWrapper, GenreItem } from "./styled";
+
 const Genres = ({ collection = [] }) => (
-  <ul>{collection.map(({ id, name }) => <li key={id}>{name}</li>)}</ul>
+  <GenresWrapper>
+    {collection.map(({ id, name }) => <GenreItem key={id}>{name}</GenreItem>)}
+  </GenresWrapper>
 );
 
 export default Genres;
---
+++ src/components/genres/styled.js
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+
+export const GenresWrapper = styled.ul``;
+export const GenreItem = styled.li``;
