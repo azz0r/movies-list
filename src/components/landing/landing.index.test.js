2f1c5aa964a1583090f059487423a04c1329e238
Aaron Lote <5314860+azz0r@users.noreply.github.com>
2018-08-24 19:14:57 +0100

    Improve testing. Added test:watch and moved some babel setup into our domain to get around create-react-app issues

--- src/components/landing/landing.index.test.js
+++ src/components/landing/landing.index.test.js
@@ -1,9 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Landing from "./landing";
+import { shallow } from "enzyme";
+import { expect } from "chai";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Landing />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Landing from "./landing";
+import Movies from "../movies";
+
+const testProps = [
+  {
+    title: "model 1",
+    overview: "overview 1",
+    backdrop_path: "/1"
+  }
+];
+
+describe("Given the Landing component", () => {
+  let component;
+  it("should render a single movie", () => {
+    const wrapper = shallow(<Landing {...testProps} />);
+    expect(wrapper.find(Movies)).to.have.length(1);
+  });
 });
